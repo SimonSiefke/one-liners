e7b0a14076a4caef471abb9a251e4263515fcb10
Simon Siefke <simon.siefke@gmail.com>
2020-09-16 18:54:42 +0200

    remove unused file

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/google-chrome-stable.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable.deb
-sudo dpkg -i /tmp/google-chrome-stable.deb
