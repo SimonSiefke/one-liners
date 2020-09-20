7dba628e31d1bffc1ee5073c9ba98a4c01cf4018
Simon Siefke <simon.siefke@gmail.com>
2020-09-20 16:58:49 +0200

    tried to add lighttable (wip_

---
+++ packages/editor.lighttable/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO not working
+
+curl https://github.com/LightTable/LightTable/releases/download/0.8.1/lighttable-0.8.1-linux.tar.gz -o /tmp/lightTable.tar.gz
+
+tar -xzf /tmp/lightTable.tar.gz
